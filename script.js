b004a8d7a76480886465ac57343099189e5a2b34
DASyhef <daspoubelle@hotmail.fr>
2023-06-06 10:32:18 +0200

    click on it

--- script.js
+++ script.js
@@ -1,18 +1,71 @@
 // Ici vous pouvez écrire du code JavaScript
 function therock() {
-    document.querySelector(".color").setAttribute('class', 'color on');
-    document.getElementById("clip-2").setAttribute('class', 'color off');
-    document.getElementById("clip-3").setAttribute('class', 'color off');
-    document.getElementById("clip-4").setAttribute('class', 'color off');
-    document.getElementById("clip-5").setAttribute('class', 'color off');
+    document.getElementById("color-1").setAttribute('class', 'color on');
+    document.getElementById("color-2").setAttribute('class', 'color off');
+    document.getElementById("color-3").setAttribute('class', 'color off');
+    document.getElementById("color-4").setAttribute('class', 'color off');
+    document.getElementById("color-5").setAttribute('class', 'color off');
+    document.getElementById("txt-1").setAttribute('class', 'whozzat on');
+    document.getElementById("txt-2").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-3").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-4").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-5").setAttribute('class', 'whozzat off');
+
 }
 document.getElementById("therock").addEventListener('click', therock)
 
 function vindiesel() {
-    document.querySelector(".color").setAttribute('class', 'color off');
-    document.getElementById("clip-2").setAttribute('class', 'color on');
-    document.getElementById("clip-3").setAttribute('class', 'color off');
-    document.getElementById("clip-4").setAttribute('class', 'color off');
-    document.getElementById("clip-5").setAttribute('class', 'color off');
+    document.getElementById("color-1").setAttribute('class', 'color off');
+    document.getElementById("color-2").setAttribute('class', 'color on');
+    document.getElementById("color-3").setAttribute('class', 'color off');
+    document.getElementById("color-4").setAttribute('class', 'color off');
+    document.getElementById("color-5").setAttribute('class', 'color off');
+    document.getElementById("txt-1").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-2").setAttribute('class', 'whozzat on');
+    document.getElementById("txt-3").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-4").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-5").setAttribute('class', 'whozzat off');
 }
-document.getElementById("therock").addEventListener('click', vindiesel)
+document.getElementById("vindiesel").addEventListener('click', vindiesel)
+
+function imenes() {
+    document.getElementById("color-1").setAttribute('class', 'color off');
+    document.getElementById("color-2").setAttribute('class', 'color off');
+    document.getElementById("color-3").setAttribute('class', 'color on');
+    document.getElementById("color-4").setAttribute('class', 'color off');
+    document.getElementById("color-5").setAttribute('class', 'color off');
+    document.getElementById("txt-1").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-2").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-3").setAttribute('class', 'whozzat on');
+    document.getElementById("txt-4").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-5").setAttribute('class', 'whozzat off');
+}
+document.getElementById("imenes").addEventListener('click', imenes)
+
+function jessicaalba() {
+    document.getElementById("color-1").setAttribute('class', 'color off');
+    document.getElementById("color-2").setAttribute('class', 'color off');
+    document.getElementById("color-3").setAttribute('class', 'color off');
+    document.getElementById("color-4").setAttribute('class', 'color on');
+    document.getElementById("color-5").setAttribute('class', 'color off');
+    document.getElementById("txt-1").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-2").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-3").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-4").setAttribute('class', 'whozzat on');
+    document.getElementById("txt-5").setAttribute('class', 'whozzat off');
+}
+document.getElementById("jessicaalba").addEventListener('click', jessicaalba)
+
+function zitahanrot() {
+    document.getElementById("color-1").setAttribute('class', 'color off');
+    document.getElementById("color-2").setAttribute('class', 'color off');
+    document.getElementById("color-3").setAttribute('class', 'color off');
+    document.getElementById("color-4").setAttribute('class', 'color off');
+    document.getElementById("color-5").setAttribute('class', 'color on');
+    document.getElementById("txt-1").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-2").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-3").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-4").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-5").setAttribute('class', 'whozzat on');
+}
+document.getElementById("zitahanrot").addEventListener('click', zitahanrot)
