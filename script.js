246b54c63492a511cd0a2a4b41de5da47e031c9d
DASyhef <daspoubelle@hotmail.fr>
2023-06-06 12:33:09 +0200

    final condom

--- script.js
+++ script.js
@@ -12,96 +12,96 @@ let t5 = document.getElementById("txt-5");
 
 
 function therock() {
-    if (getComputedStyle(c1).display != "none") {
+    if (getComputedStyle(c1).opacity != "0") {
         c1.setAttribute('class', 'color off');
-        t1.setAttribute('class', 'whozzat off')
+        t1.setAttribute('class', 'whozzat t-off')
       } else {
     document.getElementById("color-1").setAttribute('class', 'color on');
     document.getElementById("color-2").setAttribute('class', 'color off');
     document.getElementById("color-3").setAttribute('class', 'color off');
     document.getElementById("color-4").setAttribute('class', 'color off');
     document.getElementById("color-5").setAttribute('class', 'color off');
-    document.getElementById("txt-1").setAttribute('class', 'whozzat on');
-    document.getElementById("txt-2").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-3").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-4").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-5").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-1").setAttribute('class', 'whozzat t-on');
+    document.getElementById("txt-2").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-3").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-4").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-5").setAttribute('class', 'whozzat t-off');
       }
 }
 document.getElementById("therock").addEventListener('click', therock)
 
 function vindiesel() {
-    if (getComputedStyle(c2).display != "none") {
+    if (getComputedStyle(c2).opacity != "0") {
         c2.setAttribute('class', 'color off');
-        t2.setAttribute('class', 'whozzat off')
+        t2.setAttribute('class', 'whozzat t-off')
       } else {
     document.getElementById("color-1").setAttribute('class', 'color off');
     document.getElementById("color-2").setAttribute('class', 'color on');
     document.getElementById("color-3").setAttribute('class', 'color off');
     document.getElementById("color-4").setAttribute('class', 'color off');
     document.getElementById("color-5").setAttribute('class', 'color off');
-    document.getElementById("txt-1").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-2").setAttribute('class', 'whozzat on');
-    document.getElementById("txt-3").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-4").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-5").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-1").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-2").setAttribute('class', 'whozzat t-on');
+    document.getElementById("txt-3").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-4").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-5").setAttribute('class', 'whozzat t-off');
       }
 }
 document.getElementById("vindiesel").addEventListener('click', vindiesel)
 
 function imenes() {
-    if (getComputedStyle(c3).display != "none") {
+    if (getComputedStyle(c3).opacity != "0") {
         c3.setAttribute('class', 'color off');
-        t3.setAttribute('class', 'whozzat off')
+        t3.setAttribute('class', 'whozzat t-off')
       } else {
     document.getElementById("color-1").setAttribute('class', 'color off');
     document.getElementById("color-2").setAttribute('class', 'color off');
     document.getElementById("color-3").setAttribute('class', 'color on');
     document.getElementById("color-4").setAttribute('class', 'color off');
     document.getElementById("color-5").setAttribute('class', 'color off');
-    document.getElementById("txt-1").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-2").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-3").setAttribute('class', 'whozzat on');
-    document.getElementById("txt-4").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-5").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-1").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-2").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-3").setAttribute('class', 'whozzat t-on');
+    document.getElementById("txt-4").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-5").setAttribute('class', 'whozzat t-off');
       }
 }
 document.getElementById("imenes").addEventListener('click', imenes)
 
 function jessicaalba() {
-    if (getComputedStyle(c4).display != "none") {
+    if (getComputedStyle(c4).opacity != "0") {
         c4.setAttribute('class', 'color off');
-        t4.setAttribute('class', 'whozzat off')
+        t4.setAttribute('class', 'whozzat t-off')
       } else {
     document.getElementById("color-1").setAttribute('class', 'color off');
     document.getElementById("color-2").setAttribute('class', 'color off');
     document.getElementById("color-3").setAttribute('class', 'color off');
     document.getElementById("color-4").setAttribute('class', 'color on');
     document.getElementById("color-5").setAttribute('class', 'color off');
-    document.getElementById("txt-1").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-2").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-3").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-4").setAttribute('class', 'whozzat on');
-    document.getElementById("txt-5").setAttribute('class', 'whozzat off');
+    document.getElementById("txt-1").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-2").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-3").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-4").setAttribute('class', 'whozzat t-on');
+    document.getElementById("txt-5").setAttribute('class', 'whozzat t-off');
       }
 }
 document.getElementById("jessicaalba").addEventListener('click', jessicaalba)
 
 function zitahanrot() {
-    if (getComputedStyle(c5).display != "none") {
+    if (getComputedStyle(c5).opacity != "0") {
         c5.setAttribute('class', 'color off');
-        t5.setAttribute('class', 'whozzat off')
+        t5.setAttribute('class', 'whozzat t-off')
       } else {
     document.getElementById("color-1").setAttribute('class', 'color off');
     document.getElementById("color-2").setAttribute('class', 'color off');
     document.getElementById("color-3").setAttribute('class', 'color off');
     document.getElementById("color-4").setAttribute('class', 'color off');
     document.getElementById("color-5").setAttribute('class', 'color on');
-    document.getElementById("txt-1").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-2").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-3").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-4").setAttribute('class', 'whozzat off');
-    document.getElementById("txt-5").setAttribute('class', 'whozzat on');
+    document.getElementById("txt-1").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-2").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-3").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-4").setAttribute('class', 'whozzat t-off');
+    document.getElementById("txt-5").setAttribute('class', 'whozzat t-on');
       }
 }
 document.getElementById("zitahanrot").addEventListener('click', zitahanrot)
